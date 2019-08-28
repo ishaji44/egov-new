72afc398c8ffd59442bb0553e925d7aed62cf1e0
ilham <ish.haji44@gmail.com>
2019-08-28 09:50:18 +0700

    SECOND COMMIT

--- system/resources/views/home.blade.php
+++ system/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Ini adalah halaman login
                 </div>
             </div>
         </div>
